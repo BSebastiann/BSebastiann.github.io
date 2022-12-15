3f5efd69db0bbc1876b5703a04eb23ae656f7672
Sebastian Benavidez <benavideznahuel2000@gmai.com>
2022-12-14 21:06:55 -0300

    pagina terminada

---
+++ js.js
@@ -0,0 +1,8 @@
+
+
+function EnvioDatos(){
+    alert("Datos Enviados");
+    
+}
+//document.getElementById("clickMe").onclick = doFunction; 
+//document.getElementById("EnviarDatos").onclick = function () { alert('hello!'); };
